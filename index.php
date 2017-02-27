4f1ee86c1e99e137c14bd408f4e4079f8dc32914
louisxiv <502990436@qq.com>
2017-02-27 15:36:10 +0800

    微信测试

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+include './wechat-php-sdk/wechat.class.php';
+
+$options = array(
+    'token' => 'LG', //填写你设定的key
+    'encodingaeskey' => '', //填写加密用的EncodingAESKey
+    'appid' => 'wxd8e911e6cf0b7ed0', //填写高级调用功能的app id
+    'appsecret' => '87dc05c99d168869fd9ecd6f213196ef' //填写高级调用功能的密钥
+);
+$weObj = new Wechat($options);
+$weObj->valid(); //明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
+$type = $weObj->getRev()->getRevType();
+switch ($type) {
+    case Wechat::MSGTYPE_TEXT:
+        $weObj->text("hello, I'm wechat")->reply();
+        exit;
+        break;
+    case Wechat::MSGTYPE_EVENT:
+        break;
+    case Wechat::MSGTYPE_IMAGE:
+        break;
+    default:
+        $weObj->text("help info")->reply();
+}
