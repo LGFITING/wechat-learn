81041c528d83bb4e5c36838d61d89017af7823d7
louisxiv <502990436@qq.com>
2017-02-27 17:28:00 +0800

    二维码测试

--- index.php
+++ index.php
@@ -10,10 +10,14 @@ $options = array(
 );
 $weObj = new Wechat($options);
 $weObj->valid(); //明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
+
+//二维码
+$scene_id = 1;
+$getQRCode = $weObj->getQRCode($scene_id,$type=0,$expire=604800);
 $type = $weObj->getRev()->getRevType();
 switch ($type) {
     case Wechat::MSGTYPE_TEXT:
-        $weObj->text("hello, I'm wechat")->reply();
+        $weObj->text($getQRCode)->reply();
         exit;
         break;
     case Wechat::MSGTYPE_EVENT:
